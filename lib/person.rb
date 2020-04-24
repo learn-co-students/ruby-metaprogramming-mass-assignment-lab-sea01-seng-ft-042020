a12015a6faadca3621d61df6a0a3ce387c3e3a4b
aidanmc95 <aidanmc95@gmail.com>
2020-04-24 10:15:24 -0700

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+    :weight, :handed, :complexion, :t_shirt_size, 
+    :wrist_size, :glove_size, :pant_length, :pant_width
+  
+  def initialize(attributes)
+    attributes.each {|key, value| self.send("#{key}=", value)}
+  end
 end
+
+bob_attributes = {name: "Bob", hair_color: "Brown"}
+ 
+bob = Person.new(bob_attributes)
+bob.name       # => "Bob"
+bob.hair_color # => "Brown"
+ 
+susan_attributes = {name: "Susan", height: "5'11\"", eye_color: "Green"}
+ 
+susan = Person.new(susan_attributes)
+susan.name      # => "Susan"
+susan.height    # => "5'11""
+susan.eye_color # => "Green"
